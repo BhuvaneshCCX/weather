f0cc2ccbbb33e2a4ffc363ad882c594906c9f846
Bhuvanesh B <bhuvanesh.b@changecx.com>
2023-02-15 18:06:48 +0530

    15-feb-2023

--- home.js
+++ home.js
@@ -2,11 +2,11 @@ let weather = {
     apiKey: "4999544661c443baad8144328231302",
     fetchWeather: function(city) {
         fetch(
-            "http://api.weatherapi.com/v1/current.json?key="+
-            this.apiKey+
-            "&q="+
-            city+
-            "&aqi=no"
+          "http://api.weatherapi.com/v1/current.json?key="+
+          this.apiKey+
+          "&q="+
+          city+
+          "&aqi=no"
         )
         .then((response) => {
         if (!response.ok) {
@@ -19,10 +19,22 @@ let weather = {
     },
   displayWeather: function (data){
   const { name } = data.location;
-  const { temp_c } = data.current;
+  const { temp_c, temp_f } = data.current;
+  const { icon, text } = data.current.condition;
+  const { localtime } = data.location;
+  const { humidity } = data.current;
+  const { wind_kph } = data.current;
   document.querySelector(".city").innerText = "Weather in " + name;
-  document.querySelector(".temp").innerText = temp_c + "°C";
-  document.body.style.backgroundImage ="url('https://source.unsplash.com/1600x900/?" + name + "')";
+  document.querySelector(".temp").innerText = temp_c + "°C |";
+  document.querySelector(".fahr").innerText = "| " + temp_f + "°F";
+  document.querySelector(".localtime").innerText = localtime`` ;
+  document.querySelector(".icon").src = icon ;
+  document.querySelector(".description").innerText = text;
+  document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Wind speed: " + wind_kph + " km/h";
+  document.body.style.backgroundImage ="url('https://source.unsplash.com/1280x655/?" + text + "')";
 },
   search: function () {
     this.fetchWeather(document.querySelector(".search-bar").value);
