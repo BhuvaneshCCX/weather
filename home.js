4287b0aecbed15009e55ec0b96862a8944aaa090
Bhuvanesh B <bhuvanesh.b@changecx.com>
2023-02-20 22:47:37 +0530

    20-feb-2023

--- home.js
+++ home.js
@@ -1,94 +1,138 @@
-document
-  .querySelector(".search-bar")
-  .addEventListener("keyup", function (event) {
-    if (event.key == "Enter") {
-      fetchWeather();
-    }
-  });
+function fetchWeather(city) {
+  fetch(
+    "https://api.weatherapi.com/v1/forecast.json?key=bb7d76519ca8447d86b50642231302&q=" +
+      city +
+      "&days=8&aqi=yes&alerts=yes"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      let errorOccurred = false;
 
-const fetchWeather = () => {
-  var city = document.getElementById("input").value;
-  navigator.geolocation.getCurrentPosition(async (location) => {
-    var response;
-    try {
-      if (city === "") {
-        response = await fetch(
-          "https://api.weatherapi.com/v1/forecast.json?key=f233947edb16410c8b0122108231302&q=" +
-            location.coords.latitude +
-            "," +
-            location.coords.longitude +
-            "&aqi=no&days=8"
-        );
-      } else {
-        response = await fetch(
-          "https://api.weatherapi.com/v1/forecast.json?key=f233947edb16410c8b0122108231302&q=" +
-            city +
-            "&aqi=no&days=8"
-        );
+      try {
+        display(data);
+      } catch (error) {
+        if (!errorOccurred) {
+          alert("An error occurred: " + error);
+          errorOccurred = true;
+        }
       }
+    });
+}
+function display(data) {
+  console.log(data);
+  const week = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
+  const isday = data.current.is_day;
+  const code = data.current.condition.code;
 
-      let data = await response.json();
-      console.log(data);
-      document.getElementById("localtime").innerHTML = data.location.localtime;
-      const weekday = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-      ];
-      const d = data.forecast.forecastday[0].date;
-      const date = new Date(d);
-      let day = weekday[date.getDay()];
-      document.getElementById("day").innerHTML = day;
-      document.querySelector(".icon").src = data.current.condition.icon;
-      document.querySelector(".description").innerText =
-        "Weather : " + data.current.condition.text;
-      document.getElementById("city").innerHTML =
-        data.location.name + " , " + data.location.tz_id;
-      document.getElementById("temp").innerHTML =
-        data.current.temp_c + "°C / " + data.current.temp_f + "°F";
-      document.getElementById("humidity").innerHTML =
-        "Humidity : " + data.current.humidity + "%";
-      document.getElementById("wind").innerHTML =
-        "Wind speed : " + data.current.wind_kph + " km/h";
-      document.getElementById("wind-dir").innerHTML =
-        "Wind direction : " + data.current.wind_dir;
-      document.body.style.backgroundImage =
-        "url('https://source.unsplash.com/1280x655/?" +
-        data.current.condition.text +
-        "')";
-      const week = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-      ];
-      let tags = "";
-      data.forecast.forecastday.slice(1, 7).map((data) => {
-        let d = new Date(data.date);
-        console.log(week[d.getDay()], data.day.maxtemp_c, data.day.maxtemp_f);
-        tags =
-          tags +
-          `<div class="forecast col-md-2 col-6">
-          <div class="future">
-          <div id="date1">${week[d.getDay()]}</div>
-          <img src="${data.day.condition.icon}" />
-          <div id="day1tempc">${data.day.maxtemp_c} °C</div>
-          <div id="day1tempf">${data.day.maxtemp_f} °F</div>
-          </div>
-        </div>`;
-      });
-      document.getElementById("forecast").innerHTML = tags;
-    } catch (error) {
-      alert("Invalid Location");
+  document.body.style.backgroundImage =
+    "url('https://source.unsplash.com/1600x900/?" +
+    data.current.condition.text +
+    "')";
+
+  document.getElementById("city").innerHTML =
+    data.location.name + " , " + data.location.tz_id;
+  document.getElementById("localtime").innerHTML = data.location.localtime;
+  const d = data.forecast.forecastday[0].date;
+  const date = new Date(d);
+  let day = week[date.getDay()];
+  document.getElementById("day").innerHTML = day;
+  document.getElementById("temp").innerHTML =
+    data.current.temp_c + "°C / " + data.current.temp_f + "°F";
+  document.querySelector(".icon").src = data.current.condition.icon;
+  document.querySelector(".description").innerText =
+    "Weather : " + data.current.condition.text;
+  document.getElementById("humidity").innerHTML =
+    "Humidity : " + data.current.humidity + "%";
+  document.getElementById("wind").innerHTML =
+    "Wind speed : " + data.current.wind_kph + " km/h";
+  document.getElementById("wind-dir").innerHTML =
+    "Wind direction : " + data.current.wind_dir;
+
+  let tags = "";
+  data.forecast.forecastday.slice(1, 7).map((data) => {
+    let d = new Date(data.date);
+    console.log(week[d.getDay()], data.day.maxtemp_c, data.day.maxtemp_f);
+    tags =
+      tags +
+      `<div class="forecast col-md-2 col-6">
+                <div class="future">
+                <div id="date1">${week[d.getDay()]}</div>
+                <img src="${data.day.condition.icon}" />
+                <div id="day1tempc">${data.day.maxtemp_c} °C</div>
+                <div id="day1tempf">${data.day.maxtemp_f} °F</div>
+                </div>
+              </div>`;
+  });
+  document.getElementById("forecast").innerHTML = tags;
+}
+navigator.geolocation.getCurrentPosition((location) => {
+  fetch(
+    "https://api.weatherapi.com/v1/forecast.json?key=bb7d76519ca8447d86b50642231302&q=" +
+      location.coords.latitude +
+      "," +
+      location.coords.longitude +
+      "&days=7&aqi=yes"
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      fetchWeather(data.location.name);
+    });
+});
+// fetchWeather("ooty");
+const autocomplete = (city) => {
+  fetch(
+    "https://api.weatherapi.com/v1/search.json?key=f7d63b9a0a45493e9cb52439231302&&q=" +
+      city
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      var populate = document.getElementById("populate");
+      if (data) {
+        let options = data.map((location, index) => {
+          return ` <div style="cursor:pointer;" onclick="optionClicked('${
+            location.name
+          }')" class="d-grid ${
+            data.length - 1 === index ? "" : "border-bottom"
+          } py-1">
+                            <span class="fs-low">
+                                ${location.name}
+                            </span>
+                            <span class="base-text"> ${location.country}</span>
+                        </div>`;
+        });
+        if (options.length > 0) {
+          populate.setAttribute(
+            "class",
+            populate.getAttribute("class").replace(" d-none", "")
+          );
+        } else {
+          populate.setAttribute(
+            "class",
+            `${populate.getAttribute("class").replace(" d-none", "")} d-none`
+          );
+        }
+        populate.innerHTML = options.join("\n");
+      }
+    });
+};
+const optionClicked = (location) => {
+  document.getElementById("input").value = location;
+  var populate = document.getElementById("populate");
+  populate.setAttribute(
+    "class",
+    `${populate.getAttribute("class").replace(" d-none", "")} d-none`
+  );
+  fetchWeather(location);
+};
+
+const search = () => {
+  document.querySelector(".search-bar").addEventListener("keydown", (event) => {
+    if (event.key == "Enter") {
+      fetchWeather(event.target.value);
+    } else {
+      autocomplete(event.target.value);
     }
   });
 };
-
-fetchWeather();
